9f1b8cb3ffe2995c04fac558c7416bfbaf5cdc53
dmitriy <damerkulov@alfabank.ru>
2019-07-04 17:29:01 +0300

    remove unused packages

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,6 @@ module.exports = {
     entry: './src/example/index.tsx',
     module: {
         rules: [
-            // {
-            //     test: /\.(js|jsx)$/,
-            //     exclude: /node_modules/,
-            //     use: ['babel-loader'],
-            // },
             {
                 test: /\.(ts(x?)|js|jsx)$/,
                 exclude: /node_modules/,
@@ -18,15 +13,6 @@ module.exports = {
                     {
                         loader: "ts-loader"
                     }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: [
-                    process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
                 ]
             },
             {enforce: 'pre', test: /\.js$/, loader: 'source-map-loader'}
@@ -43,13 +29,7 @@ module.exports = {
     },
     devtool: process.env.NODE_ENV !== 'production' ? 'source-map' : false,
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        })
+        new webpack.HotModuleReplacementPlugin()
     ],
     devServer: {
         contentBase: './www/static',
